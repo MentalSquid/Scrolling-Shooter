1ef66727e1d84a0ec3fe58101a9777859cb0ec14
Mentalsquid-Ampere <maliqnor+gitAmpere@gmail.com>
2018-12-10 18:19:01 +0100

    first commit

---
+++ app/src/main/java/mentalsquid/scrolling_shooter/GameActivity.java
@@ -0,0 +1,12 @@
+package mentalsquid.scrolling_shooter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
